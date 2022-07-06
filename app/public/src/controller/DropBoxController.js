2c41af670ebf64d443b995eba86569fbca28fda8
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-07-06 11:36:10 -0300

    Add: Deleting FireBase file

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -49,12 +49,19 @@ class DropBoxController {
       let file = JSON.parse(li.dataset.file);
       let key = li.dataset.key;
 
-      let formData = new FormData()
+      promises.push(new Promise((resolve, reject) => {
+        let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.name);
 
-      formData.append('path', file.path);
-      formData.append('key', key);
-
-      promises.push(this.ajax('/file', 'DELETE', formData));
+        fileRef.delete().then(() => {
+          resolve({
+            fields: {
+              key
+            }
+          });
+        }).catch(err => {
+          reject(err);
+        });
+      }));
 
     });
     
